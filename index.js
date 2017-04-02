d12d3262c8823db2233f2a6b898fbd870adb669e
jef4490 <jeffrey.hechler@gmail.com>
2017-04-02 10:05:14 -0400

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+let twoGrannies = React.createElement('p', {}, 'Two grannies having the time of their life!')
+let passengers = React.createElement('p', {}, 'Passengers:')
+let agnes = React.createElement('li', {}, 'Agnes')
+let muriel = React.createElement('li', {}, 'Muriel')
+let peeps = React.createElement('ul', {}, [agnes, muriel])
+
+const OlderCoaster = React.createClass({
+  render() {
+    return React.createElement('div', {className: 'oldercoaster'}, [twoGrannies, passengers, peeps])
+  }
+})
+
+let youShouldnt = React.createElement('p', {}, "You shouldn't look too far.")
+let rightInFront = React.createElement('strong', {}, "right in front of you.")
+let sometimes = React.createElement('p', {}, ["Sometimes, the solution is ", rightInFront])
+
+class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {}, [youShouldnt, sometimes])
+  }
+}
+
+let hello = React.createElement('p', {}, "Hello! We have the following products for sale today:")
+let butcherList = React.createElement('ul', {}, BUTCHER_PRODUCTS.map((meat) => {
+  return React.createElement('li', {}, meat)
+}))
+
+class ButcherShop extends React.Component {
+  render() {
+    return React.createElement('div', {className: 'butcher-shop'}, [hello, butcherList])
+  }
+}
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
